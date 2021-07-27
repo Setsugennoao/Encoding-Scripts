9eab9ac87c5b204f96470f009d490b3c894a71de
Setsugennoao <matteoeoe@gmail.com>
2021-07-27 21:55:01 +0200

    Automagically get default path

--- [himawari] Boruto - Naruto Next Generations/boruto_commons/utils.py
+++ [himawari] Boruto - Naruto Next Generations/boruto_commons/utils.py
@@ -1,6 +1,18 @@
+import inspect
 import vapoursynth as vs
 from vsutil import depth
 
 
 def resize_spline(clip: vs.VideoNode) -> vs.VideoNode:
   return depth(clip.resize.Spline64(1280, 720), 16)
+
+
+def get_ep_number(filename: str):
+  return filename.split('_')[-1].split('.')[0]
+
+
+def get_default_path(filename: str = None):
+  if filename is None:
+    filename = inspect.stack()[1].filename
+  ep_num = get_ep_number(filename)
+  return fr"G:\Drive condivisi\Fansub\_Other\[himawari] Boruto - Naruto Next Generations\{ep_num}\[Crunchyroll] Boruto - Naruto Next Generations - {ep_num} (1080p).mkv"
