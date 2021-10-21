ab476ad293c65b43e7d8637da4f0318369fdc493
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2021-10-21 19:43:08 +0200

    SelePro Update Graining and x265 settings

--- SELECTION PROJECT/selection_project_commons/constants.py
+++ SELECTION PROJECT/selection_project_commons/constants.py
@@ -6,7 +6,7 @@ seed = random.seed()
 graigasm_args = dict(
     thrs=[x << 8 for x in (26, 75, 130, 180)],
     strengths=[(0.7, 0.2), (1, 0.12), (1.05, 0.05), (0.12, 0)],
-    sizes=(1.1, 1.13, 1.46, 1.5),
+    sizes=(1.1, 1.53, 1.86, 1.7),
     sharps=(80, 60, 40, 40),
     grainers=[
         AddGrain(seed=seed, constant=False),
