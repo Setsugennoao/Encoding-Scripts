a824feb09fd7bb6ec7c9052c246cac40b8ac1f55
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-06-01 13:29:23 +0200

    Kyou no Aqours: format files

--- Setsugen/Kyou no Aqours [WEB]/kyounoaquaa_commons/encoding.py
+++ Setsugen/Kyou no Aqours [WEB]/kyounoaquaa_commons/encoding.py
@@ -9,27 +9,27 @@ core = vs.core
 
 
 class Encoding:
-  runner: SelfRunner
+    runner: SelfRunner
 
-  def __init__(self, file: FileInfo, clip: vs.VideoNode, prefetch: Optional[int] = None) -> None:
-    self.file = file
-    self.clip = clip
-    self.v_encoder = X265('kyounoaquaa_commons/x265_settings')
-    if prefetch:
-      self.v_encoder.prefetch = prefetch
+    def __init__(self, file: FileInfo, clip: vs.VideoNode, prefetch: Optional[int] = None) -> None:
+        self.file = file
+        self.clip = clip
+        self.v_encoder = X265('kyounoaquaa_commons/x265_settings')
+        if prefetch:
+            self.v_encoder.prefetch = prefetch
 
-  def run(self,) -> None:
-    assert self.file.a_src, self.file.a_src_cut
+    def run(self,) -> None:
+        assert self.file.a_src, self.file.a_src_cut
 
-    muxer = Mux(
-        self.file,
-        streams=(
-            VideoStream(self.file.name_clip_output, 'Encoded by Setsugen no ao', JAPANESE),
-            AudioStream(self.file.a_src, 'YouTube Stereo OPUS', JAPANESE),
-            None
+        muxer = Mux(
+            self.file,
+            streams=(
+                VideoStream(self.file.name_clip_output, 'Encoded by Setsugen no ao', JAPANESE),
+                AudioStream(self.file.a_src, 'YouTube Stereo OPUS', JAPANESE),
+                None
+            )
         )
-    )
 
-    config = RunnerConfig(self.v_encoder, None, None, None, None, muxer)
+        config = RunnerConfig(self.v_encoder, None, None, None, None, muxer)
 
-    SelfRunner(self.clip, self.file, config).run()
+        SelfRunner(self.clip, self.file, config).run()
--- Setsugen/Kyou no Aqours [WEB]/kyounoaquaa_commons/filtering.py
+++ Setsugen/Kyou no Aqours [WEB]/kyounoaquaa_commons/filtering.py
@@ -13,50 +13,51 @@ core.max_cache_size = 16 * 2 ** 10
 
 
 class KyouNoAquaaFiltering:
-  YOUTUBE: FileInfo
+    YOUTUBE: FileInfo
 
-  def __init__(self, YOUTUBE: FileInfo):
-    self.YOUTUBE = YOUTUBE
+    def __init__(self, YOUTUBE: FileInfo):
+        self.YOUTUBE = YOUTUBE
 
-  @finalise_output()
-  def filterchain(self):
-    src = self.mix_source()
+    @finalise_output()
+    def filterchain(self):
+        src = self.mix_source()
 
-    y = depth(get_y(src), 16)
-    src_444 = src.resize.Bicubic(format=vs.YUV444P16)
+        y = depth(get_y(src), 16)
+        src_444 = src.resize.Bicubic(format=vs.YUV444P16)
 
-    deblock_mask = kgf.kirsch(self.get_dctmask(y, 0.5))
-    denoise_mask = self.get_dctmask(y, 0.34).std.BinarizeMask(27 << 8).std.Deflate().std.Inflate().std.Inflate().std.Deflate()
+        deblock_mask = kgf.kirsch(self.get_dctmask(y, 0.5))
+        denoise_mask = self.get_dctmask(y, 0.34).std.BinarizeMask(
+            27 << 8).std.Deflate().std.Inflate().std.Inflate().std.Deflate()
 
-    deblock = self.deblock(src_444)
-    denoise = self.denoise(y)
+        deblock = self.deblock(src_444)
+        denoise = self.denoise(y)
 
-    masked = src_444.std.MaskedMerge(deblock, deblock_mask)
+        masked = src_444.std.MaskedMerge(deblock, deblock_mask)
 
-    masked = masked.std.MaskedMerge(merge_chroma(denoise, src_444), denoise_mask)
+        masked = masked.std.MaskedMerge(merge_chroma(denoise, src_444), denoise_mask)
 
-    masked = masked.resize.Bicubic(
-        format=vs.YUV420P16, filter_param_a_uv=-0.5, filter_param_b_uv=0.25
-    )
+        masked = masked.resize.Bicubic(
+            format=vs.YUV420P16, filter_param_a_uv=-0.5, filter_param_b_uv=0.25
+        )
 
-    grain = self.graining(masked, denoise_mask)
+        grain = self.graining(masked, denoise_mask)
 
-    return grain
+        return grain
 
-  def mix_source(self):
-    return self.YOUTUBE.clip_cut
+    def mix_source(self):
+        return self.YOUTUBE.clip_cut
 
-  def get_dctmask(self, y: vs.VideoNode, lumi: float):
-    return y.dctf.DCTFilter((lumi, 1, 0.75, 0.25, 0.5, 0.5, 0.12, 0))
+    def get_dctmask(self, y: vs.VideoNode, lumi: float):
+        return y.dctf.DCTFilter((lumi, 1, 0.75, 0.25, 0.5, 0.5, 0.12, 0))
 
-  def deblock(self, clip: vs.VideoNode) -> vs.VideoNode:
-    return haf.SMDegrain(clip, 2, 750, 250, True, True)
+    def deblock(self, clip: vs.VideoNode) -> vs.VideoNode:
+        return haf.SMDegrain(clip, 2, 750, 250, True, True)
 
-  def denoise(self, y: vs.VideoNode) -> vs.VideoNode:
-    try:
-      return BM3DCuda(y, 2.44, 1, Profile.LC, refine=2).clip
-    except vs.Error:
-      return BM3D(y, 1.356, 1, Profile.LC, refine=2).clip
+    def denoise(self, y: vs.VideoNode) -> vs.VideoNode:
+        try:
+            return BM3DCuda(y, 2.44, 1, Profile.LC, refine=2).clip
+        except vs.Error:
+            return BM3D(y, 1.356, 1, Profile.LC, refine=2).clip
 
-  def graining(self, clip: vs.VideoNode, mask: vs.VideoNode) -> vs.VideoNode:
-    return clip.grain.Add(0.37, 0.01).std.MaskedMerge(clip, mask)
+    def graining(self, clip: vs.VideoNode, mask: vs.VideoNode) -> vs.VideoNode:
+        return clip.grain.Add(0.37, 0.01).std.MaskedMerge(clip, mask)
